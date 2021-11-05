95bd63f8114f5a5748b9feaee19324579b995209
TGRush <58338843+TGRush@users.noreply.github.com>
2021-11-06 00:54:41 +0100

    Make prep.sh automatically remove temp.txt

--- prep.sh
+++ prep.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Welcome to the most retarded script in existence which I only wrote as I'm too lazy to sort out issues myself rE
 awk -v n=1 'NR==FNR{total=NR;next} FNR==total-n+1{exit} 1' app/src/main/res/xml/grayscale_icon_map.xml app/src/main/res/xml/grayscale_icon_map.xml >> temp.txt && mv temp.txt app/src/main/res/xml/grayscale_icon_map.xml
+rm temp.txt
 cat patches.xml >> app/src/main/res/xml/grayscale_icon_map.xml
 echo "added strings to grayscale_icon_map.xml"
 echo "adding VectorGraphics or whatever they're called.."
