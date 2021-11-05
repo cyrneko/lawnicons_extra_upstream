f565ef82f94d8af7b9e7b547b98f7d1f0e56826c
TGRush <58338843+TGRush@users.noreply.github.com>
2021-11-06 00:00:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 awk -v n=1 'NR==FNR{total=NR;next} FNR==total-n+1{exit} 1' app/src/main/res/xml/grayscale_icon_map.xml app/src/main/res/xml/grayscale_icon_map.xml >> temp.txt && mv temp.txt app/src/main/res/xml/grayscale_icon_map.xml
 cat patches.xml >> app/src/main/res/xml/grayscale_icon_map.xml
 echo "added strings to grayscale_icon_map.xml"
-echo "adding VectorGraphics or whatever they're called..."
-/usr/bin/git clone https://github.com/TGRush/lawnicons_extra_upstream.git -b Vectors --progress 
-mv ./lawnicons_extra_upstream/* ./app/src/main/res/drawable/
-rm -rf ./lawnicons_extra_upstream
+echo "adding VectorGraphics or whatever they're called.."
+mv ./vectors/* ./app/src/main/res/drawable/
 rm -rf "./app/src/main/res/drawable/.git"
-echo "try building now :)"
+echo "try building normally now :>"
