6238eefc92f2e7b7744715acfe4cd64bf5d4fb19
TGRush <58338843+TGRush@users.noreply.github.com>
2021-11-06 00:57:30 +0100

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -2,9 +2,11 @@
 # Welcome to the most retarded script in existence which I only wrote as I'm too lazy to sort out issues myself rE
 awk -v n=1 'NR==FNR{total=NR;next} FNR==total-n+1{exit} 1' app/src/main/res/xml/grayscale_icon_map.xml app/src/main/res/xml/grayscale_icon_map.xml >> temp.txt && mv temp.txt app/src/main/res/xml/grayscale_icon_map.xml
 rm temp.txt
-cat patches.xml >> app/src/main/res/xml/grayscale_icon_map.xml
+cat patches.xml >> ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml
 echo "added strings to grayscale_icon_map.xml"
 echo "adding VectorGraphics or whatever they're called.."
-mv ./vectors/* ./app/src/main/res/drawable/
-rm -rf "./app/src/main/res/drawable/.git"
+mv ./vectors/* ./lawnicons/app/src/main/res/drawable/
+rm -rf "./lawnicons/app/src/main/res/drawable/.git"
 echo "try building normally now :>"
+
+# i need to stop making small stupid mistakes...
