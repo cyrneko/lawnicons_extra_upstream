c5a3bd3a41c6dbfefff9a1a3cac445ec786e9240
TGRush <58338843+TGRush@users.noreply.github.com>
2021-11-06 01:03:39 +0100

    should've thought of that

--- prep.sh
+++ prep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Welcome to the most retarded script in existence which I only wrote as I'm too lazy to sort out issues myself rE
 awk -v n=1 'NR==FNR{total=NR;next} FNR==total-n+1{exit} 1' ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml >> temp.txt && mv temp.txt ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml
-rm temp.txt
+rm temp.txt 2> /dev/null
 cat patches.xml >> ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml
 echo "added strings to grayscale_icon_map.xml"
 echo "adding VectorGraphics or whatever they're called.."
