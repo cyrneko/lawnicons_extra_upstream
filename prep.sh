3a21f38b0a954d759ff1d257da9ee3a1971694a7
TGRush <58338843+TGRush@users.noreply.github.com>
2021-11-06 01:02:14 +0100

    I honestly feel like i have dementia at this point

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Welcome to the most retarded script in existence which I only wrote as I'm too lazy to sort out issues myself rE
-awk -v n=1 'NR==FNR{total=NR;next} FNR==total-n+1{exit} 1' ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml >> temp.txt && mv temp.txt app/src/main/res/xml/grayscale_icon_map.xml
+awk -v n=1 'NR==FNR{total=NR;next} FNR==total-n+1{exit} 1' ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml >> temp.txt && mv temp.txt ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml
 rm temp.txt
 cat patches.xml >> ./lawnicons/app/src/main/res/xml/grayscale_icon_map.xml
 echo "added strings to grayscale_icon_map.xml"
